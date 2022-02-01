b6f83116b717c5a698b1ef3e1f50ced4eec59479
JAGAN S <81976809+Jagan-Pro@users.noreply.github.com>
2022-02-01 20:54:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple Calendar Using PHP and JavaScript</title>
+</head>
+<body>
+    <center>
+    <h1>Simple Calendar Using PHP and JAVASCRIP</h1>
+    </center>
+    <ul>
+        <li><a href="showcalendar.php" type="text/php">Calendar without events</a></li><br>
+        <li><a href="showcalendar_ withevent.php" type="text/php">Calendar with events using PHP & MYSQL</a></li><br>
+        <li><a href="showcalendar_inJS.html">Calendar without events using JAVASCRIP</a></li><br>
+
+    </ul>
+
+ 
+</body>
+</html>
